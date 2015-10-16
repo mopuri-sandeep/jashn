c09a6f6c966a660a9c0ec87618ea2ccae54e6842
Srijan Gupta <srijan02420@gmail.com>
2015-10-16 18:57:53 +0530

    get heights for songs

--- app/src/main/java/com/jashn/app/MainActivity.java
+++ app/src/main/java/com/jashn/app/MainActivity.java
@@ -1,13 +1,334 @@
 package com.jashn.app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.jashn.app.wave.WaveformView;
+import com.jashn.app.wave.soundfile.SoundFile;
+import com.ringdroid.R;
+
+import java.io.File;
+
+public class MainActivity extends Activity {
+
+    private long mLoadingLastUpdateTime;
+    private boolean mLoadingKeepGoing;
+    private long mRecordingLastUpdateTime;
+    private boolean mRecordingKeepGoing;
+    private double mRecordingTime;
+    private boolean mFinishActivity;
+    private TextView mTimerTextView;
+    private AlertDialog mAlertDialog;
+    private ProgressDialog mProgressDialog;
+    private SoundFile mSoundFile;
+    private File mFile;
+    private String mFilename;
+    private String mArtist;
+    private String mTitle;
+    private int mNewFileKind;
+    private boolean mWasGetContentIntent;
+    private TextView mStartText;
+    private TextView mEndText;
+    private TextView mInfo;
+    private String mInfoContent;
+    private ImageButton mPlayButton;
+    private ImageButton mRewindButton;
+    private ImageButton mFfwdButton;
+    private boolean mKeyDown;
+    private String mCaption = "";
+    private int mWidth;
+    private int mMaxPos;
+    private int mStartPos;
+    private WaveformView mWaveformView;
+    private int mEndPos;
+    private boolean mStartVisible;
+    private boolean mEndVisible;
+    private int mLastDisplayedStartPos;
+    private int mLastDisplayedEndPos;
+    private int mOffsetGoal;
+    private int mFlingVelocity;
+    private int mPlayStartMsec;
+    private int mPlayEndMsec;
+    private Handler mHandler;
+    private boolean mIsPlaying;
+    private boolean mTouchDragging;
+    private float mTouchStart;
+    private int mTouchInitialOffset;
+    private int mTouchInitialStartPos;
+    private int mTouchInitialEndPos;
+    private long mWaveformTouchStartMsec;
+    private float mDensity;
+    private int mMarkerLeftInset;
+    private int mMarkerRightInset;
+    private int mMarkerTopOffset;
+    private int mMarkerBottomOffset;
+
+    private int[] mLenByZoomLevel;
+    private double[][] mValuesByZoomLevel;
+    private double[] mZoomFactorByZoomLevel;
+    private int[] mHeightsAtThisZoomLevel;
+    private int mZoomLevel;
+    private int mNumZoomLevels;
+    private int mSampleRate;
+    private int mSamplesPerFrame;
+    private int mOffset;
+
+    private Thread mLoadSoundFileThread;
+    private Thread mRecordAudioThread;
+    private Thread mSaveSoundFileThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+
+        mWasGetContentIntent = intent.getBooleanExtra("was_get_content_intent", false);
+
+        mFilename = intent.getData().toString().replaceFirst("file://", "").replaceAll("%20", " ");
+        mSoundFile = null;
+        mHandler = new Handler();
+
+        loadFromFile();
+
+//        mWaveformView = (WaveformView)findViewById(R.id.waveform);
+//        mWaveformView.setListener(this);
+//
+//        mInfo = (TextView)findViewById(R.id.info);
+//        mInfo.setText(mCaption);
+//
+//        mMaxPos = 0;
+//        mLastDisplayedStartPos = -1;
+//        mLastDisplayedEndPos = -1;
+//
+//        if (mSoundFile != null && !mWaveformView.hasSoundFile()) {
+//            mWaveformView.setSoundFile(mSoundFile);
+//            mWaveformView.recomputeHeights(mDensity);
+//            mMaxPos = mWaveformView.maxPos();
+//        }
+    }
+
+    private void loadFromFile() {
+        mFile = new File(mFilename);
+
+        mLoadingLastUpdateTime = getCurrentTime();
+        mLoadingKeepGoing = true;
+        mFinishActivity = false;
+        mProgressDialog = new ProgressDialog(MainActivity.this);
+        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        mProgressDialog.setTitle(R.string.progress_dialog_loading);
+        mProgressDialog.setCancelable(true);
+        mProgressDialog.setOnCancelListener(
+                new DialogInterface.OnCancelListener() {
+                    public void onCancel(DialogInterface dialog) {
+                        mLoadingKeepGoing = false;
+                        mFinishActivity = true;
+                    }
+                });
+        mProgressDialog.show();
+
+        final SoundFile.ProgressListener listener =
+                new SoundFile.ProgressListener() {
+                    public boolean reportProgress(double fractionComplete) {
+                        long now = getCurrentTime();
+                        if (now - mLoadingLastUpdateTime > 100) {
+                            mProgressDialog.setProgress(
+                                    (int) (mProgressDialog.getMax() * fractionComplete));
+                            mLoadingLastUpdateTime = now;
+                        }
+                        return mLoadingKeepGoing;
+                    }
+                };
+
+        // Load the sound file in a background thread
+        mLoadSoundFileThread = new Thread() {
+            public void run() {
+                try {
+                    Log.e("path", mFile.getAbsolutePath());
+                    mSoundFile = SoundFile.create(mFile.getAbsolutePath(), listener);
+
+                    if (mSoundFile == null) {
+                        mProgressDialog.dismiss();
+                        String name = mFile.getName().toLowerCase();
+                        String[] components = name.split("\\.");
+                        String err;
+                        if (components.length < 2) {
+                            err = getResources().getString(
+                                    R.string.no_extension_error);
+                        } else {
+                            err = getResources().getString(
+                                    R.string.bad_extension_error) + " " +
+                                    components[components.length - 1];
+                        }
+                        final String finalErr = err;
+                        Runnable runnable = new Runnable() {
+                            public void run() {
+//                                showFinalAlert(new Exception(), finalErr);
+                            }
+                        };
+                        mHandler.post(runnable);
+                        return;
+                    }
+//                    mPlayer = new SamplePlayer(mSoundFile);
+                } catch (final Exception e) {
+                    mProgressDialog.dismiss();
+                    e.printStackTrace();
+                    mInfoContent = e.toString();
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            mInfo.setText(mInfoContent);
+                        }
+                    });
+
+                    Runnable runnable = new Runnable() {
+                        public void run() {
+//                            showFinalAlert(e, getResources().getText(R.string.read_error));
+                        }
+                    };
+                    mHandler.post(runnable);
+                    return;
+                }
+                mProgressDialog.dismiss();
+                if (mLoadingKeepGoing) {
+                    Runnable runnable = new Runnable() {
+                        public void run() {
+                            finishOpeningSoundFile();
+                        }
+                    };
+                    mHandler.post(runnable);
+                } else if (mFinishActivity){
+//                    finish();
+                }
+            }
+        };
+        mLoadSoundFileThread.start();
+    }
+
+    private void finishOpeningSoundFile() {
+        computeDoublesForAllZoomLevels();
+        computeIntsForThisZoomLevel();
+        Log.e("array", mHeightsAtThisZoomLevel.toString());
+    }
+
+    private void computeDoublesForAllZoomLevels() {
+        int numFrames = mSoundFile.getNumFrames();
+        int[] frameGains = mSoundFile.getFrameGains();
+        double[] smoothedGains = new double[numFrames];
+        if (numFrames == 1) {
+            smoothedGains[0] = frameGains[0];
+        } else if (numFrames == 2) {
+            smoothedGains[0] = frameGains[0];
+            smoothedGains[1] = frameGains[1];
+        } else if (numFrames > 2) {
+            smoothedGains[0] = (double)(
+                    (frameGains[0] / 2.0) +
+                            (frameGains[1] / 2.0));
+            for (int i = 1; i < numFrames - 1; i++) {
+                smoothedGains[i] = (double)(
+                        (frameGains[i - 1] / 3.0) +
+                                (frameGains[i    ] / 3.0) +
+                                (frameGains[i + 1] / 3.0));
+            }
+            smoothedGains[numFrames - 1] = (double)(
+                    (frameGains[numFrames - 2] / 2.0) +
+                            (frameGains[numFrames - 1] / 2.0));
+        }
+
+        // Make sure the range is no more than 0 - 255
+        double maxGain = 1.0;
+        for (int i = 0; i < numFrames; i++) {
+            if (smoothedGains[i] > maxGain) {
+                maxGain = smoothedGains[i];
+            }
+        }
+        double scaleFactor = 1.0;
+        if (maxGain > 255.0) {
+            scaleFactor = 255 / maxGain;
+        }
+
+        // Build histogram of 256 bins and figure out the new scaled max
+        maxGain = 0;
+        int gainHist[] = new int[256];
+        for (int i = 0; i < numFrames; i++) {
+            int smoothedGain = (int)(smoothedGains[i] * scaleFactor);
+            if (smoothedGain < 0)
+                smoothedGain = 0;
+            if (smoothedGain > 255)
+                smoothedGain = 255;
+
+            if (smoothedGain > maxGain)
+                maxGain = smoothedGain;
+
+            gainHist[smoothedGain]++;
+        }
+
+        // Re-calibrate the min to be 5%
+        double minGain = 0;
+        int sum = 0;
+        while (minGain < 255 && sum < numFrames / 20) {
+            sum += gainHist[(int)minGain];
+            minGain++;
+        }
+
+        // Re-calibrate the max to be 99%
+        sum = 0;
+        while (maxGain > 2 && sum < numFrames / 100) {
+            sum += gainHist[(int)maxGain];
+            maxGain--;
+        }
+
+        // Compute the heights
+        double[] heights = new double[numFrames];
+        double range = maxGain - minGain;
+        for (int i = 0; i < numFrames; i++) {
+            double value = (smoothedGains[i] * scaleFactor - minGain) / range;
+            if (value < 0.0)
+                value = 0.0;
+            if (value > 1.0)
+                value = 1.0;
+            heights[i] = value * value;
+        }
+
+        mNumZoomLevels = 1;
+        mLenByZoomLevel = new int[mNumZoomLevels];
+        mZoomFactorByZoomLevel = new double[mNumZoomLevels];
+        mValuesByZoomLevel = new double[mNumZoomLevels][];
+
+        mZoomLevel = 0;
+
+        // Level 0 is doubled, with interpolated values
+        mLenByZoomLevel[0] = numFrames * 2;
+        mZoomFactorByZoomLevel[0] = 2.0;
+        mValuesByZoomLevel[0] = new double[mLenByZoomLevel[0]];
+        if (numFrames > 0) {
+            mValuesByZoomLevel[0][0] = 0.5 * heights[0];
+            mValuesByZoomLevel[0][1] = heights[0];
+        }
+        for (int i = 1; i < numFrames; i++) {
+            mValuesByZoomLevel[0][2 * i] = 0.5 * (heights[i - 1] + heights[i]);
+            mValuesByZoomLevel[0][2 * i + 1] = heights[i];
+        }
+    }
+
+    private void computeIntsForThisZoomLevel() {
+        int halfHeight = 100;
+        mHeightsAtThisZoomLevel = new int[mLenByZoomLevel[mZoomLevel]];
+        for (int i = 0; i < mLenByZoomLevel[mZoomLevel]; i++) {
+            mHeightsAtThisZoomLevel[i] =
+                    (int)(mValuesByZoomLevel[mZoomLevel][i] * halfHeight);
+        }
+    }
+
+    private long getCurrentTime() {
+        return System.nanoTime() / 1000000;
     }
 }
